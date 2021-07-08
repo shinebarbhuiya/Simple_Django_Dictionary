1946e4945f7526f329bd71bec807d9aab6ca59e6
Shine <shinealom@gmail.com>
2021-07-09 00:25:23 +0530

    edited the setting file

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -26,8 +26,6 @@ urlpatterns = [
     path('', include('weatherApp.urls')),
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
     
 ]
